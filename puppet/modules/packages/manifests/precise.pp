d8b4a5fe48869133e7e12a3e2585468407699303
Espen Braastad <espen@varnish-software.com>
2014-05-12 14:40:34 +0200

    Deps

--- puppet/modules/packages/manifests/precise.pp
+++ puppet/modules/packages/manifests/precise.pp
@@ -2,7 +2,8 @@ class packages::precise {
 
     $packages = [ 'libwww-perl', 'strace', 'lsof', 'vim', 
         'emacs', 'build-essential', 'devscripts', 'debhelper', 
-        'git', 'pylint', 'python-nose', 'apt-transport-https' ]
+        'git', 'pylint', 'python-nose', 'apt-transport-https', 'pkg-config',
+        'libtool', 'automake' ]
     
     package {
         $packages:
