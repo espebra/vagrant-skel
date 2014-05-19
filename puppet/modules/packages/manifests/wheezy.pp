5e736a516e54a91732e4ae79d39e74e28b2c8364
Espen Braastad <espen@varnish-software.com>
2014-05-19 13:20:28 +0200

    packages

--- puppet/modules/packages/manifests/wheezy.pp
+++ puppet/modules/packages/manifests/wheezy.pp
@@ -1,8 +1,8 @@
 class packages::wheezy {
 
-    $packages = [ 'libwww-perl', 'strace', 'lsof', 'vim', 
-        'emacs', 'build-essential', 'devscripts', 'debhelper', 
-        'git', 'pylint', 'python-nose', 'apt-transport-https' ]
+    $packages = [ 'libwww-perl', 'strace', 'lsof', 'vim', 'automake',
+        'emacs', 'build-essential', 'devscripts', 'debhelper', 'libtool', 
+        'git', 'pylint', 'python-nose', 'apt-transport-https', 'pkg-config' ]
     
     package {
         $packages:
