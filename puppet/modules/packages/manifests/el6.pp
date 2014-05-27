6ed7f7476c7995a69062a4a09df3b6bd5e1d3864
Espen Braastad <espen@varnish-software.com>
2014-05-27 08:49:58 +0200

    rst2pdf in el6 for varnish

--- puppet/modules/packages/manifests/el6.pp
+++ puppet/modules/packages/manifests/el6.pp
@@ -2,7 +2,7 @@ class packages::el6 {
 
     $packages = [ 'perl-libwww-perl', 'strace', 'lsof', 'vim-enhanced', 
                   'emacs', 'rpm-build', 'automake', 'autoconf',
-                  'rpmdevtools', 'rpmlint', 'rpm-devel', 'git', 
+                  'rpmdevtools', 'rpmlint', 'rpm-devel', 'git', 'rst2pdf',
                   'pylint', 'python-nose', 'libtool', 'pcre-devel' ]
 
     package {
