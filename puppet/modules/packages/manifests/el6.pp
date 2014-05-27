79217f5e9d0d7e8844dd6708a3308f69968ce9bd
Espen Braastad <espen@varnish-software.com>
2014-05-27 08:41:38 +0200

    libtool in el6

--- puppet/modules/packages/manifests/el6.pp
+++ puppet/modules/packages/manifests/el6.pp
@@ -3,7 +3,7 @@ class packages::el6 {
     $packages = [ 'perl-libwww-perl', 'strace', 'lsof', 'vim-enhanced', 
                   'emacs', 'rpm-build', 'automake', 'autoconf',
                   'rpmdevtools', 'rpmlint', 'rpm-devel', 'git', 
-                  'pylint', 'python-nose' ]
+                  'pylint', 'python-nose', 'libtool' ]
 
     package {
         $packages:
