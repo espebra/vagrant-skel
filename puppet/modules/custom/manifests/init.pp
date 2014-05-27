b2b05941ce829e246f8aac4115b5ec7aec41a613
Espen Braastad <espen@varnish-software.com>
2014-05-27 08:42:05 +0200

    output to screen from the custom module

--- puppet/modules/custom/manifests/init.pp
+++ puppet/modules/custom/manifests/init.pp
@@ -1,19 +1,20 @@
 class custom {
+    Exec {
+        path      => "/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin",
+        logoutput => true,
+        timeout   => 900,
+    }
     exec {
         "/vagrant/init.d/init ; touch /var/tmp/init":
-            path    => "/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin",
             onlyif  => "test -x /vagrant/init.d/init",
             creates => "/var/tmp/init";
         "/vagrant/init.d/init.${operatingsystem} ; touch /var/tmp/init.${operatingsystem}":
-            path    => "/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin",
             onlyif  => "test -x /vagrant/init.d/init.${operatingsystem}",
             creates => "/var/tmp/init.${operatingsystem}";
         "/vagrant/init.d/init.${lsbdistcodename} ; touch /var/tmp/init.${lsbdistcodename}":
-            path    => "/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin",
             onlyif  => "test -x /vagrant/init.d/init.${lsbdistcodename}",
             creates => "/var/tmp/init.${lsbdistcodename}";
         "/vagrant/init.d/init.${hostname} ; touch /var/tmp/init.${hostname}":
-            path    => "/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin",
             onlyif  => "test -x /vagrant/init.d/init.${hostname}",
             creates => "/var/tmp/init.${hostname}";
     }
