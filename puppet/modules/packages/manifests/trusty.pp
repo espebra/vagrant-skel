fcbc21be171369e5aeb81bd14fa68660c7a8f164
Espen Braastad <espen@varnish-software.com>
2014-09-22 14:49:41 +0200

    adding more packages

--- puppet/modules/packages/manifests/trusty.pp
+++ puppet/modules/packages/manifests/trusty.pp
@@ -2,7 +2,9 @@ class packages::trusty {
 
     $packages = [ 'libwww-perl', 'strace', 'lsof', 'vim', 'curl',
         'emacs', 'build-essential', 'devscripts', 'debhelper', 
-        'git', 'pylint', 'python-nose', 'apt-transport-https' ]
+        'git', 'pylint', 'python-nose', 'apt-transport-https', 'libtool',
+        'autoconf', 'libpcre3-dev', 'pkg-config', 'm4', 'libreadline-dev',
+        'python-docutils' ]
     
     package {
         $packages:
